4a8e555d16dfb11c4402285fe4abb5907670fa14
MomentRing <904372358@qq.com>
2022-05-21 10:15:06 +0800

    完成了主页

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,7 +2,22 @@
 //todo 会先调用 ajaxPrefilter 这个函数
 //todo 在这个函数中，可以拿到我们给Ajax提供的配置对象
 
-$.ajaxPrefilter((option) => {
-     //* 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-    option.url = 'http://www.liulongbin.top:3007' + option.url
+$.ajaxPrefilter((options) => {
+    //* 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+    options.url = 'http://www.liulongbin.top:3007' + options.url
+    if (options.url.includes('/my/')) {
+        // *给有权限的借口注入token
+        options.headers = {
+            Authorization: localStorage.getItem('token'),
+        };
+    }
+    //* 不论成功还是失败，最终都会调用 complete 回调函数
+    options.complete = (res) => {
+        //* 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 &&
+            res.responseJSON.message === "身份认证失败！") {
+            localStorage.removeItem('token');
+            location.href = '/login.html';
+        }
+    }
 })
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,5 @@
 $(function () {
+    // !登录注册功能开发
     //* 点击去注册账号让 登录框隐藏，注册框显示
     $('#link_reg').click(() => {
         $('.login-box').hide();
@@ -10,61 +11,62 @@ $(function () {
         $('.login-box').show();
         $(".reg-box").hide();
     })
-})
 
-// 从 LayUI 中获取 form 对象
-const form = layui.form;
-// 通过 form.verify() 方法自定义校验规则
-form.verify({
-    // 自定义一个叫 pwd 的校验规则
-    pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
-    // 校验两次密码是否一致的规则
-    repwd: (val) => {
-        const pwd = $(".reg-box [name=repassword]").val();
-        if (val !== pwd) return "两次密码不一致"
-    }
-})
-
-// 获取 layui 弹窗
-const layer = layui.layer;
-// 设置请求根路径
-
-// todo 监听注册表单，发送注册请求
-$('#form_reg').on('submit', function (e) {
-    // 阻止表单默认提交事件
-    e.preventDefault();
-    $.ajax({
-        type: 'POST',
-        url: "/api/reguser",
-        data: {
-            username: $('#form_reg [name="username"]').val(),
-            password: $('#form_reg [name="password"]').val(),
-        },
-        success: function (res) {
-            if (res.status !== 0) {
-                return layer.msg('注册失败')
-            }
-            layer.msg('注册成功')
-            // 注册成功后跳转到登录界面
-            $('#link_login').click()
+    // 从 LayUI 中获取 form 对象
+    const form = layui.form;
+    // 通过 form.verify() 方法自定义校验规则
+    form.verify({
+        // 自定义一个叫 pwd 的校验规则
+        pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
+        // 校验两次密码是否一致的规则
+        repwd: (val) => {
+            const pwd = $(".reg-box [name=repassword]").val();
+            if (val !== pwd) return "两次密码不一致"
         }
     })
-})
-//todo 监听登录表单，发送登录请求
-$('#form_login').on('submit', function (e) {
-    e.preventDefault();
-    $.ajax({
-        type: 'POST',
-        url: "/api/login",
-        data: $(this).serialize(),
-        success: function (res) {
-            if (res.status !== 0) {
-                return layer.msg('登录失败')
-            }
-            layer.msg("登录成功！");
-            localStorage.setItem("token", res.token);
-            location.href = '/index.html';
 
-        }
+    // 获取 layui 弹窗
+    const layer = layui.layer;
+    // 设置请求根路径
+
+    // todo 监听注册表单，发送注册请求
+    $('#form_reg').on('submit', function (e) {
+        // 阻止表单默认提交事件
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: "/api/reguser",
+            data: {
+                username: $('#form_reg [name="username"]').val(),
+                password: $('#form_reg [name="password"]').val(),
+            },
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('注册失败')
+                }
+                layer.msg('注册成功')
+                // 注册成功后跳转到登录界面
+                $('#link_login').click()
+            }
+        })
+    })
+    //todo 监听登录表单，发送登录请求
+    $('#form_login').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: "/api/login",
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败')
+                }
+                layer.msg("登录成功！");
+                localStorage.setItem("token", res.token);
+                location.href = '/index.html';
+
+            }
+        })
     })
 })
+
